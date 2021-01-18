55bfee04475d0fbe60891d62faebaceaafdf40e8
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-18 17:09:34 +0530

    second

--- config/conn.js
+++ config/conn.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 module.exports = conn = mysql.createConnection({
     host: "localhost",
-    user: "yourusername",
-    password: "yourpassword",
-    database: "mydb"
+    user: "wissenaire_sudheer",
+    password: "sudheer@wissenaire",
+    database: "wissenaire_ca21"
   });
