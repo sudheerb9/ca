53b3714f7200bb85baa6803037199dd02dd3b616
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-26 18:53:14 +0530

    ok

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser= require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var session = require('cookie-session');
-const FacebookStrategy = require('passport-facebook').Strategy;
-require('./config/config');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('./config/conn');
+require('./config/config');
 
 var indexRouter = require('./routes/index');
 
@@ -16,10 +17,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -30,8 +30,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
-
 passport.serializeUser(function(user,done){
   done(null, user);
 });
@@ -40,59 +38,66 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
   
-passport.use (new FacebookStrategy({
+passport.use (new GoogleStrategy({
         clientID: config.FACEBOOK_APP_ID,
         clientSecret: config.FACEBOOK_APP_SECRET,
-        callbackURL: "https://ca.wissenaire.org/auth/facebook/callback"
+        callbackURL: "https://ca21.wissenaire.org/auth/facebook/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         console.log('Hi this is passport using facebook strategy');
-        conn.connect(function(err) {
-            if (err) throw err;
+        
+        query = "SELECT * FROM users WHERE facebookid = "+profile.id+"";
+        conn.connect((err) => {
+          if (err) throw err;
+          console.log('Connected!');
+        });
+        conn.query(query,function(err,user){
+          console.log('query executed');
+            if (err)
+                return done(err);
+            else if (user) {
+                console.log(user);
+                return done(null, user);
+            } 
             else {
-                query = "SELECT * FROM users WHERE facebookid = "+profile.id+"";
-                conn.query(query,function(err,user){
-                
-                    if (err)
-                        return done(err);
-                    else if (user) {
-                        console.log('user');
-                        return done(null, user);
-                    } 
-                    else {
-                        console.log('else');
-                        var insertquery = "INSERT INTO users (name, email, facebookid, photo, accesstoken, refreshtoken) \
-                        VALUES ( "+profile.displayName+", "+profile.emails[0].value+", "+profile.id+", "+profile.photos[0].value+", "+accessToken+", "+refreshToken+" ) "
-                        conn.query(insertquery, function (err, result) {
-                            if (err) throw err;
-                            console.log(result);
-                        });
-                        console.log(profile.emails[0].value);
-                        return done(null, user);
-                    }
+                console.log('else');
+                var insertquery = "INSERT INTO users (name, email, facebookid, photo, accesstoken, refreshtoken) \
+                VALUES ( '"+profile.displayName+"', '"+profile.emails[0].value+"', '"+profile.id+"', '"+profile.photos[0].value+"', '"+accessToken+"', '"+refreshToken+"' ) "
+                conn.query(insertquery, function (err, result) {
+                    if (err) throw err;
+                    console.log(result);
                 });
+                console.log(profile.emails[0].value);
+                return done(null, user);
             }
+        });
+        conn.end((err) => {
+          // The connection is terminated gracefully
+          // Ensures all remaining queries are executed
+          // Then sends a quit packet to the MySQL server.
         })
+            
     }
 ));
 
-app.get('/auth/facebook', passport.authenticate('facebook', {
-scope: ['public_profile', 'email']}), (req, res) => {
+app.use('/', indexRouter);
+
+app.get('/auth/facebook', passport.authenticate('google',  { scope : ['profile', 'email'] }), (req, res) => {
   console.log('In auth route');
 })
 
-app.get('/auth/facebook/callback', 
-passport.authenticate('facebook', {successRedirect:'/home', failureRedirect: '/auth/facebook' }),
-(req, res) => {
-  console.log('Hi this is callback');
-  alert('Team wissenaire welcomes you !')
-  
-})
+app.get('/auth/facebook/callback', passport.authenticate('google', { successRedirect:'/home', failureRedirect: '/auth/google'}) ,
+  (req, res) => {
+      console.log("login done");
+      res.redirect('/home');
+  }
+);
 
-router.get('/logout', (req, res) => {
+app.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/')
 })
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config/conn.js
+++ config/conn.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 
-module.exports = conn = mysql.createConnection({
+module.exports = conn = mysql.createPool({
     host: "localhost",
     user: "wissenaire_sudheer",
     password: "sudheer@wissenaire",
