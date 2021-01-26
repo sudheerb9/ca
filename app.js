5d3838634e435b4d89a7b45fc8dc280badabe2e0
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-26 15:30:44 +0530

    ok

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var session = require('cookie-session');
+const FacebookStrategy = require('passport-facebook').Strategy;
+require('./config/config');
+require('./config/conn');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,7 +32,68 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use(usersRouter);
+
+passport.serializeUser(function(user,done){
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+  
+passport.use (new FacebookStrategy({
+        clientID: config.FACEBOOK_APP_ID,
+        clientSecret: config.FACEBOOK_APP_SECRET,
+        callbackURL: "https://ca.wissenaire.org/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log('Hi this is passport using facebook strategy');
+        conn.connect(function(err) {
+            if (err) throw err;
+            else {
+                query = "SELECT * FROM users WHERE facebookid = "+profile.id+"";
+                conn.query(query,function(err,user){
+                
+                    if (err)
+                        return done(err);
+                    else if (user) {
+                        console.log('user');
+                        return done(null, user);
+                    } 
+                    else {
+                        console.log('else');
+                        var insertquery = "INSERT INTO users (name, email, facebookid, photo, accesstoken, refreshtoken) \
+                        VALUES ( "+profile.displayName+", "+profile.emails[0].value+", "+profile.id+", "+profile.photos[0].value+", "+accessToken+", "+refreshToken+" ) "
+                        conn.query(insertquery, function (err, result) {
+                            if (err) throw err;
+                            console.log(result);
+                        });
+                        console.log(profile.emails[0].value);
+                        return done(null, user);
+                    }
+                });
+            }
+        })
+    }
+));
+
+app.get('/auth/facebook', passport.authenticate('facebook', {
+scope: ['public_profile', 'email']}), (req, res) => {
+  console.log('In auth route');
+})
+
+app.get('/auth/facebook/callback', 
+passport.authenticate('facebook', {successRedirect:'/home', failureRedirect: '/auth/facebook' }),
+(req, res) => {
+  console.log('Hi this is callback');
+  alert('Team wissenaire welcomes you ! ${user.name}')
+  
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy;
-require('../config/conn');
-require('../config/config');
+
 
   console.log('Hi this is passport');
-  passport.serializeUser(function(user,done){
-		done(null, user);
-	});
+  
 
-	passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-  });
-    
-  passport.use (new FacebookStrategy({
-          clientID: config.FACEBOOK_APP_ID,
-          clientSecret: config.FACEBOOK_APP_SECRET,
-          callbackURL: "https://ca.wissenaire.org/auth/facebook/callback"
-      },
-      function(accessToken, refreshToken, profile, done) {
-          console.log('Hi this is passport using facebook strategy');
-          conn.connect(function(err) {
-              if (err) throw err;
-              else {
-                  query = "SELECT * FROM users WHERE facebookid = "+profile.id+"";
-                  conn.query(query,function(err,user){
-                  
-                      if (err)
-                          return done(err);
-                      else if (user) {
-                          console.log('user');
-                          return done(null, user);
-                      } 
-                      else {
-                          console.log('else');
-                          var insertquery = "INSERT INTO users (name, email, facebookid, photo, accesstoken, refreshtoken) \
-                          VALUES ( "+profile.displayName+", "+profile.emails[0].value+", "+profile.id+", "+profile.photos[0].value+", "+accessToken+", "+refreshToken+" ) "
-                          conn.query(insertquery, function (err, result) {
-                              if (err) throw err;
-                              console.log(result);
-                          });
-                          console.log(profile.emails[0].value);
-                          return done(null, user);
-                      }
-                  });
-              }
-          })
-      }
-  ));
-
-router.get('/auth/facebook', passport.authenticate('facebook', {
-  scope: ['public_profile', 'email']}), (req, res) => {
-    console.log('In auth route');
-  })
-
-router.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
-  (req, res) => {
-    console.log('Hi this is callback');
-    res.redirect('/home')
-  })
-
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
 
 module.exports = router;
