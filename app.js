d5f2cde011a0c53fee03098239d72ab2dff4cad0
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-28 15:40:30 +0530

    ok

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.use (new FacebookStrategy({
   });
 }));
 
-app.get('/auth/facebook', passport.authenticate('facebook', { authType: 'reauthenticate'}));
+app.get('/auth/facebook', passport.authenticate('facebook', { authType: 'reauthenticate', scope: ['public_profile']}));
 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/home', failureRedirect: '/', failureFlash: true }),
     function(req, res) {
