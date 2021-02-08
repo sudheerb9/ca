a4a60e42963bd1517dd1aa8a4dc452d2a7b819f4
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-02-08 12:05:49 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var request = require('request');
 
 const conn = mysql.createPool({
   host: "localhost",
-  user: "wissenaire_sudheer",
-  password: "sudheer@wissenaire",
+  user: "root",
+  password: "",
   database: "wissenaire_ca21"
 });
 
@@ -14,15 +15,24 @@ function ensureAuthenticated(req, res, next) {
     console.log('loggedin')
     return next();
   }
-  res.redirect('/login')
+  res.redirect('/auth/facebook')
 }
 
+function ensureProfile(req,res,next){
+  const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
+  conn.query(qr, (err, rows) => {
+    if(err) throw err;
+    console.log(rows[0])
+    if(rows[0].wissid) return next();
+    res.redirect ('/profile');
+  })
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/home',ensureAuthenticated, async function(req, res, next) {
+router.get('/home',ensureAuthenticated, ensureProfile, async function(req, res, next) {
   const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
   conn.query(qr, (err, rows) => {
     if(err) throw err;
@@ -31,7 +41,7 @@ router.get('/home',ensureAuthenticated, async function(req, res, next) {
   })
 });
 
-router.get('/fbshare',ensureAuthenticated, function(req, res, next) {
+router.get('/fbshare',ensureAuthenticated, ensureProfile, function(req, res, next) {
   const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
   conn.query(qr, (err, rows) => {
     if(err) throw err;
@@ -45,7 +55,7 @@ router.get('/fbshare',ensureAuthenticated, function(req, res, next) {
   })
 });
 
-router.get('/ideate',ensureAuthenticated, async function(req, res, next) {
+router.get('/ideate',ensureAuthenticated, ensureProfile, async function(req, res, next) {
   const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
   conn.query(qr, (err, rows) => {
     if(err) throw err;
@@ -54,30 +64,38 @@ router.get('/ideate',ensureAuthenticated, async function(req, res, next) {
   })
 });
 
-router.get('/leaderboard',ensureAuthenticated, function(req, res, next) {
+router.get('/sharecon',ensureAuthenticated, ensureProfile, function(req, res, next) {
   const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
   conn.query(qr, (err, rows) => {
     if(err) throw err;
     console.log(rows[0])
-    res.render('leaderboard', {participant : rows[0]})
+    res.render('sharecontact', {participant : rows[0]})
   })
 });
 
-router.get('/contact',ensureAuthenticated, function(req, res, next) {
+router.get('/leaderboard',ensureAuthenticated, ensureProfile, function(req, res, next) {
+  const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
+  conn.query(qr, (err, rows) => {
+    if(err) throw err;
+    console.log(rows[0])
+    const select = ("SELECT name, wissid, institute, points FROM users ORDER BY users.points DESC");
+    conn.query(select, (err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      var rank = result.map(function(o) { return o.wissid; }).indexOf(rows[0].wissid);
+      console.log(rank);
+      rows[0].rank = rank;
+      res.render('leaderboard', {participant : rows[0],leaderboard :result});
+    })
+  })
+});
+
+router.get('/contact',ensureAuthenticated, ensureProfile, function(req, res, next) {
   const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
   conn.query(qr, (err, rows) => {
     if(err) throw err;
     console.log(rows[0])
     res.render('contact', {participant : rows[0]})
-  })
-});
-
-router.get('/activity',ensureAuthenticated, function(req, res, next) {
-  const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
-  conn.query(qr, (err, rows) => {
-    if(err) throw err;
-    console.log(rows[0])
-    res.render('activity', {participant : rows[0]})
   })
 });
 
@@ -89,6 +107,62 @@ router.get('/profile',ensureAuthenticated, function(req, res, next) {
     res.render('profile', {participant : rows[0]})
   })
 });
+
+router.post('/profile',function(req,res,next) {
+  // request.post(
+  // {
+  //   url:'https://maker.ifttt.com/use/nAMtzUz7KSIrj0glp1sYmdiekK6sYJepE149usjnt4c',
+  //   json: {
+  //     key1:req.body.phone,
+  //     key2:req.body.name
+  //   },
+  //   headers: {'Content-Type': 'application/json'}
+  // },
+  // function(error, response, body){
+  //   console.log(body);
+  // });
+  if(req.body.refca){
+    const qr = ("UPDATE users SET wissid = '"+req.body.wissid+"', email2 ='"+req.body.email2+"', phone = '"+req.body.phone+"', gender = '"+req.body.gender+"', institute = '"+req.body.institute+"', year = '"+req.body.year+"', city = '"+req.body.city+"', whyca = '"+req.body.whyca+"' , howca = '"+req.body.howca+"' , ref = '"+req.body.refca+"' WHERE email = '"+req.user.emails[0].value+"' ;");
+    conn.query(qr, (err, rows)=>{
+      if(err) throw err;
+      console.log(rows);
+      const part = ("SELECT points from users WHERE wissid = '"+req.body.refca+"' ;");
+      conn.query(part, (err,data) =>{
+        if(err) throw err;
+        if(data) {
+          console.log(data[0].points);
+          var points = data[0].points + 25;
+          const update = ("UPDATE `users` SET points = '"+points+"' WHERE wissid = '"+req.body.refca+"';");
+          conn.query(update, (err,result)=>{
+            if (err) throw err;
+            console.log(result);
+            res.redirect('/profile')
+          })
+        }
+        else {
+          console.log('no ca found')
+          res.redirect('/profile');
+        }
+      })
+    })
+  }
+  else if(req.body.ref){
+    const qr = ("UPDATE users SET wissid = '"+req.body.wissid+"', email2 ='"+req.body.email2+"', phone = '"+req.body.phone+"', gender = '"+req.body.gender+"', institute = '"+req.body.institute+"', year = '"+req.body.year+"', city = '"+req.body.city+"', whyca = '"+req.body.whyca+"' , howca = '"+req.body.howca+"' , ref = '"+req.body.ref+"' WHERE email = '"+req.user.emails[0].value+"' ;");
+    conn.query(qr, (err, rows)=>{
+      if(err) throw err;
+      console.log(rows);
+      res.redirect('/profile');
+    })
+  }
+  else {
+    const qr = ("UPDATE users SET wissid = '"+req.body.wissid+"', email2 ='"+req.body.email2+"', phone = '"+req.body.phone+"', gender = '"+req.body.gender+"', institute = '"+req.body.institute+"', year = '"+req.body.year+"', city = '"+req.body.city+"', whyca = '"+req.body.whyca+"' , howca = '"+req.body.howca+"' , ref = '' WHERE email = '"+req.user.emails[0].value+"' ;");
+    conn.query(qr, (err, rows)=>{
+      if(err) throw err;
+      console.log(rows);
+      res.redirect('/profile');
+    })
+  }
+})
 
 router.get('/post', function(req,res,next){
   res.render('addpost');
@@ -107,7 +181,33 @@ router.post('/addpost', function(req,res,next){
       res.sendStatus(200)
     })
   })
-  
+})
+
+router.post('/increase/:id', function(req,res,next){
+  var postid = req.params.id;
+  const qr = ("SELECT * from users where email ='" + req.user.emails[0].value + "';");
+  conn.query(qr, (err, rows) => {
+    if(err) throw err;
+    var points  = rows[0].points + 10;
+    const increasepost = ("UPDATE `users` SET points = '"+points+"', '"+postid+"' = 1 WHERE email ='" + req.user.emails[0].value + "';");
+    conn.query(increasepost, (err, result)=>{
+      if(err) throw err;
+      console.log(result);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.send('callback(\'{\"msg\": \"OK\"}\')');
+    })
+  })
+})
+
+router.put('/wissid/:id', function(req, res,next){
+  var wissid = req.params.id;
+  console.log('id'+wissid)
+  const qr = ("SELECT name from `users` WHERE wissid = '"+wissid+"';");
+  conn.query(qr, (err,result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send(result[0]);
+  })
 })
 
 module.exports = router;
